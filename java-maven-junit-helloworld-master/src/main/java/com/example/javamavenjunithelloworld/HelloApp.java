352154979b4427c61942872494bb240fd1f44ef9
vinay <vipadala@cisco.com>
2019-08-04 12:11:34 +0530

    Removed Whitespaces

--- java-maven-junit-helloworld-master/src/main/java/com/example/javamavenjunithelloworld/HelloApp.java
+++ java-maven-junit-helloworld-master/src/main/java/com/example/javamavenjunithelloworld/HelloApp.java
@@ -6,7 +6,7 @@ package com.example.javamavenjunithelloworld;
  */
 public class HelloApp {
 
-	static int DEFAULT_TIMES = 3;
+    static int DEFAULT_TIMES = 3;
 
 	static int EXIT_STATUS_PARAMETER_NOT_UNDERSTOOD = 2;
 	static int EXIT_STATUS_HELLO_FAILED = 4;
@@ -24,9 +24,9 @@ public class HelloApp {
 			try {
 				times = Integer.valueOf(args[0]);
 			} catch (NumberFormatException e) {
-				System.err.println("I don't understand the parameter you passed me. Is it a number? "
-						+ "Parameter was: [" + args[0] + "]");
-				System.exit(EXIT_STATUS_PARAMETER_NOT_UNDERSTOOD);
+				System.err.println("I don't understand the parameter you."
+						+ "Pam Was: [" + args[0] + "]");
+				System.exit(EXIT_STATUS_PARAMS_NOT_UNDERSTOOD);
 			}
 		}
 
@@ -34,7 +34,7 @@ public class HelloApp {
 		try {
 			hi.setTimes(times);
 		} catch (IllegalArgumentException e) {
-			System.err.println("Something went wrong: " + e.getMessage());
+			System.err.println("Smethingwntwrng:" + e.getMessage());
 			System.exit(EXIT_STATUS_HELLO_FAILED);
 		}
 		hi.sayHello(System.out);
